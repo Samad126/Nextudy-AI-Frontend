1c4c4d0eab7c3d41c07c9635450db005809fab93
Samad126 <samed192005@gmail.com>
2026-03-28 06:16:47 +0400

    feat: sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,28 @@
+import { withSentryConfig } from "@sentry/nextjs"
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "standalone",
-    reactStrictMode: false,
-    webpack: (config) => {
-        config.resolve.alias.canvas = false
-        return config
+  output: "standalone",
+  reactStrictMode: false,
+  webpack: (config) => {
+    config.resolve.alias.canvas = false
+    return config
+  },
+  turbopack: {
+    resolveAlias: {
+      canvas: "./empty-module.js",
     },
-    turbopack: {
-        resolveAlias: {
-            canvas: "./empty-module.js",
-        },
-    },
+  },
 }
 
-export default nextConfig
+export default withSentryConfig(nextConfig, {
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  webpack: {
+    treeshake: {
+      removeDebugLogging: true,
+    },
+  },
+})
