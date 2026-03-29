d18d34cf42a908db9a501673b701dddcd6b45d2e
Samad126 <samed192005@gmail.com>
2026-03-29 18:54:44 +0400

    fix 2 for cookie cleaning

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -2,6 +2,12 @@ import { NextResponse } from "next/server"
 
 export async function POST() {
   const response = NextResponse.json({ message: "Logout successfull" })
-  response.cookies.delete("refreshToken")
+  response.cookies.set("refreshToken", "", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+    path: "/",
+    maxAge: 0,
+  })
   return response
 }
