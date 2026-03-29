76334c83e4e871660fdbbbdd1f3b695777c7ae24
Samad126 <samed192005@gmail.com>
2026-03-29 18:59:47 +0400

    fix 3 for token cleaning issue

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,13 +1,17 @@
 import { NextResponse } from "next/server"
 
 export async function POST() {
-  const response = NextResponse.json({ message: "Logout successfull" })
+  const isProd = process.env.NODE_ENV === 'production'
+  const response = NextResponse.json({ message: "Logout successful" })
+
   response.cookies.set("refreshToken", "", {
     httpOnly: true,
-    secure: true,
+    secure: isProd,
     sameSite: "lax",
     path: "/",
     maxAge: 0,
+    ...(isProd && { domain: '.alakbaroff.com' }),
   })
+
   return response
 }
